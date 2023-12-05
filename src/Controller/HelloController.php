e4299112892038bb530806a876d4668edb68e840
0xOthmane <0xothodor@proton.me>
2023-12-05 13:02:06 +0100

    feat: ORM

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\DTO\ContactDTO;
+use App\Entity\Post;
 use App\Form\ContactFormType;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,8 +16,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class HelloController extends AbstractController
 {
     #[Route('/hello/{name}', name: 'app_hello')]
-    public function index(Request $request, string $name): Response
+    public function index(Request $request, string $name, EntityManagerInterface $manager, PostRepository $repository): Response
     {
+        $post = new Post();
+        $post->setCreatedAt(new \DateTimeImmutable());
+        $post->setUpdatedAt(new \DateTimeImmutable());
+        $post->setTitle('Title');
+        $post->setSlug('post');
+        $post->setContent('Content');
+
+        // dd($manager);
+        $manager->persist($post);
+        $manager->flush();
+
+        $posts = $manager->getRepository(Post::class)->findAll();
+        // $posts = $manager->getRepository(Post::class)->findBy('column', 'condition');
+        $post = $repository->findOrFailBySlug('post');
+        $post->setTitle('Other title');
+        // $manager->remove($post);
+        $manager->flush();
         $data = new ContactDTO();
         $form = $this->createForm(ContactFormType::class, $data);
         $form->handleRequest($request);
