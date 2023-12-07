252960629c97635aefc2a3598deaf37084a82ff7
0xOthmane <0xothodor@proton.me>
2023-12-07 14:38:19 +0100

    feat: service & dependency injection

---
+++ src/RemoteApiService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+class RemoteApiService{
+    public function __construct(
+        #[Autowire('%demo_api_key%')]
+        public string $key)
+    {
+        
+    }
+}
